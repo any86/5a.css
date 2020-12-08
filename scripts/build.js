d844ce737fc505b4009491497af8a561da52bad9
any86 <383514580@qq.com>
2020-12-08 13:39:48 +0800

    refactor: 拆分var/reset子包

--- scripts/build.js
+++ scripts/build.js
@@ -4,6 +4,7 @@ const shell = require('shelljs');
 const path = require('path');
 // 打包
 build('packages/5a.css/src/main.scss', 'packages/5a.css/dist/css/5a.css');
+build('packages/reset/src/reset.scss', 'packages/reset/dist/reset.css');
 build('packages/helpers/src/helpers.scss', 'packages/helpers/dist/helpers.css');
 
 /**
